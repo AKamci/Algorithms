b9bbd52abb274f18f5da292aade971bb2c8b7b6c
AKamci <76794253+Scavaliers@users.noreply.github.com>
2024-02-02 21:33:48 +0300

    Added merge sort and merge sort test.

---
+++ Algorithms.Test/Sorting/MergeSort.Tests/MergeSortTests.cs
@@ -0,0 +1,21 @@
+namespace MergeSort.Tests
+{
+    public class MergeSortTests
+    {
+        [Fact]
+        public void Should_Sort_Array_By_Using_Merge_Sort()
+        {
+            var unSortedArr = new int[] { 12, 45, 7, 23, 56, 89, 43, 2 };
+            int[] oldArr = new int[7];
+            Array.Copy(unSortedArr, oldArr, 7);
+
+            MergeSort mergeSort = new MergeSort();
+            mergeSort.sort(unSortedArr, 0, unSortedArr.Length - 1);
+            
+            Assert.NotEqual(oldArr, unSortedArr);
+
+        }
+    
+
+    }
+}
